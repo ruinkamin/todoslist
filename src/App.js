6206090ff5143a742afb5babb0a956f515ef0d4e
ruinkamin <mariiiaa.kolm@gmail.com>
2026-02-26 01:10:03 +0200

    hw3

--- src/App.js
+++ src/App.js
@@ -1,37 +1,106 @@
-import React, { useState } from "react";
+import React from "react";
+import TodoList from "./ToDoList";
 import './App.css'; 
 
-function ToDoApp() {
-  const [inputValue, setInputValue] = useState("");
-  const [todos, setTodos] = useState([]);
+const beginTodo = [
+    { id: 1, name: "1" },
+    { id: 2, name: "2" },
+    { id: 3, name: "3" },
+];
 
-  function addTodo() {
+class ToDoApp extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      todos: JSON.parse(localStorage.getItem("todos")) || beginTodo,
+      inputValue: ""
+    };
+
+  }
+
+  addTodo = () => {
+    const { todos, inputValue } = this.state;
+
     if (inputValue.trim() !== "") {
-      setTodos([...todos, inputValue]);
-      setInputValue("")
+      const newTodo = {
+        id: todos.length ? todos[todos.length - 1].id + 1 : 1,
+        name: inputValue
+      };
+
+      const newTodos = [...todos, newTodo];
+
+      this.setState(
+        { todos: newTodos, inputValue: "" }, () => {
+          localStorage.setItem("todos", JSON.stringify(this.state.todos));
+        }
+      );
     }
   }
 
-  return (
-    <div>
-      <h1>My ToDoApp</h1>
-      <input type="text" value={inputValue}
-        onChange={(e) => setInputValue(e.target.value)}
-        placeholder="input todo"
-        onKeyDown={(e) => e.key === "Enter" && addTodo()}></input>
-      
-      <p>{todos.length}</p>
-      
-      <ul>
-        {todos.map((todo, index) => (
-          <li key={index}>{todo}</li>
-        ))}
-      </ul>
-      
-      <button onClick={addTodo}>Add</button>
-    </div>
-  );
-}
+  deleteToDo = (id) => {
+    const newTodos = this.state.todos.filter(todo => todo.id !== id);
+    this.setState(
+      { todos: newTodos }, () => {
+        localStorage.setItem("todos", JSON.stringify(this.state.todos));
+      }
+    );
+  }
 
+  clearTodo = () => {
+    this.setState({ todos: [] }, () => {
+        localStorage.removeItem("todos");
+      }
+    );
+  }
+
+  handelInputChange = (e) => {
+    this.setState({ inputValue: e.target.value });
+  }
+
+  handelKeyDown = (e) => {
+    if (e.key === "Enter") {
+      this.addTodo();
+    }
+  }
+
+  render() {
+    const { todos, inputValue } = this.state;
+    return (
+      <div className="obj">
+        <input
+          className="inputTodo"
+          type="text"
+          value={inputValue}
+          onChange={this.handelInputChange}
+          onKeyDown={this.handelKeyDown}
+        />
+        <p className="num">{todos.length}</p>
+
+        <TodoList
+          todos={todos}
+          onDelete={this.deleteToDo}
+        />
+
+        <button
+          className="mainBT"
+          onClick={this.addTodo}
+        >
+          Add ToDo
+        </button>
+
+        <button
+          className="mainBT"
+          onClick={this.clearTodo}
+        >
+          Clear All ToDos
+        </button>
+
+      </div>
+
+    )
+  }
+}
 
 export default ToDoApp;
---
+++ src/ToDoList.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+function TodoList({ todos, onDelete }) {
+
+    return (
+
+        <ul className="tasks">
+
+            {todos.map((todo) => (
+
+                <li className="task" key={todo.id}>
+
+                    {todo.name}
+
+                    <button
+                        className="delBT"
+                        onClick={() => onDelete(todo.id)}
+                    >
+                        ✕
+                    </button>
+
+                </li>
+
+            ))}
+
+        </ul>
+
+    )
+
+}
+
+export default TodoList;
